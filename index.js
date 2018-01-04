6e6d9dce4e1fc79f02875a1d80133c2719b619ef
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 14:43:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(driver) {
+  drivers.push(driver)
+}
+
+function destructivelyPrependDriver(driver) {
+  drivers.unshift(driver)
+}
